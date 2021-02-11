f9fa463ed774f69626718a1117b8a6e639b62e24
carolZapata <carozapatac@gmail.com>
2021-02-11 20:19:51 +0100

    [ADD] Insertar alumno con spring en controlador

--- src/main/java/com/carol/colegio/controladores/AlumnoController.java
+++ src/main/java/com/carol/colegio/controladores/AlumnoController.java
@@ -1,7 +1,6 @@
 package com.carol.colegio.controladores;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,12 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.carol.colegio.dao.ComboDAO;
-import com.carol.colegio.dtos.ComboDTO;
-import com.carol.colegio.entities.MunicipioEntity;
-import com.carol.colegio.repositorios.MunicipioRepository;
-
+import com.carol.colegio.entities.AlumnoEntity;
+import com.carol.colegio.repositorios.AlumnoRepository;
 
 @Controller
 public class AlumnoController {
@@ -22,16 +21,35 @@ public class AlumnoController {
 	@Autowired
 	private ComboDAO combo;
 	
+	@Autowired
+	private AlumnoRepository alumnoRepository;
+	
 	//private static final Logger logger = LoggerFactory.getILoggerFactory(AlumnoController.class);
 	
 	@GetMapping(value = "insertaralumno")
 	public String formularioInsertarAlumno(ModelMap model) {
 		model.addAttribute("listaMunicipios", combo.comboMunicipios());
 		
-		return "vistas/alumnos/insertarAlumnos";
-		
+		return "vistas/alumnos/insertarAlumnos";		
 	}
 	
+	@PostMapping(value = "insertaralumno")
+	public String InsertarAlumno(@RequestParam(value = "id", required = false) Integer id, 
+			@RequestParam(value = "nombre") String nombre, @RequestParam(value = "municipios") Integer idMunicipio,
+			@RequestParam(value = "familiaNumerosa", required = false) Integer familiaNumerosa, 
+			ModelMap model){
+		
+		familiaNumerosa = (familiaNumerosa == null) ? 0 : 1;
+		
+		AlumnoEntity alumno = new AlumnoEntity(id, nombre, idMunicipio, familiaNumerosa);
+		
+		alumnoRepository.save(alumno);
+		
+		model.addAttribute("listaMunicipios", combo.comboMunicipios());
+		
+		return "vistas/alumnos/insertarAlumnos";	
+		
+	}
 	
 	
 	
